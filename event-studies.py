55e031879381f8e4a04205ef07cd50affa8f5850
JohnUmbaugh <john.umbaugh@gmail.com>
2016-04-06 21:10:56 -0400

    refactor: eventfinder.py

--- event-studies.py
+++ event-studies.py
@@ -1,21 +1,11 @@
-import pandas as pd
 import numpy as np
 import math
-import copy
 import QSTK.qstkutil.qsdateutil as du
 import datetime as dt
 import QSTK.qstkutil.DataAccess as da
 import eventprofiler as ep
+import eventfinder
 import portfoliosim
-
-class DiscreteEvent:
-	def __init__( self, timestamp_index, symbol, price ):
-		self.timestamp_index = timestamp_index
-		self.symbol = symbol
-		self.price = price
-
-	def to_string( self, ldt_timestamps ):
-		return "Event|" + str( self.timestamp_index ) + "|" + str( ldt_timestamps[ self.timestamp_index ] ) + "|" + self.symbol + "|" + str(self.price)
 
 class Order:
 	def __init__( self, order_id, order_group_id, timestamp_index, symbol, share_count, order_type ):
@@ -29,77 +19,6 @@ class Order:
 	def to_string( self, ldt_timestamps ):
 		return "Order|" + str( self.order_id ) + "|" + str( self.order_group_id ) + "|" + str( ldt_timestamps[ self.timestamp_index ] ) \
 			+ "|" + self.symbol + "|" + str( self.share_count ) + "|" + self.order_type
-
-def find_events( ls_symbols, d_data, ldt_timestamps, qualifier ):
-	''' Finding the event dataframe '''
-	df_close = d_data['close']
-	ts_market = df_close['SPY']
-
-	discrete_events = []
-	window = 20
-
-	spy_rolling_mean = pd.stats.moments.rolling_mean( ts_market, window)
-	spy_rolling_std = pd.stats.moments.rolling_std( ts_market, window)
-	spy_bollinger_values = ( ts_market - spy_rolling_mean ) / spy_rolling_std
-
-	print "Finding Events"
-
-	# Creating an empty dataframe
-	event_matrix = copy.deepcopy(df_close)
-	event_matrix = event_matrix * np.NAN
-
-	for s_sym in ls_symbols:
-		rolling_mean = pd.stats.moments.rolling_mean( df_close[s_sym], window)
-		rolling_std = pd.stats.moments.rolling_std( df_close[s_sym], window)
-		bollinger_values = ( df_close[s_sym] - rolling_mean ) / rolling_std
-
-		value_dict = {
-			"bollinger values" : bollinger_values,
-			"spy bollinger values" : spy_bollinger_values,
-			"rolling std" : rolling_std,
-			"close prices" : df_close[s_sym].ix
-		}
-
-		for i in range(1, len(ldt_timestamps)):
-			# Calculating the returns for this timestamp
-			f_symprice_today = df_close[s_sym].ix[ldt_timestamps[i]]
-			f_symprice_yest = df_close[s_sym].ix[ldt_timestamps[i - 1]]
-
-			if ( qualifier( i, ldt_timestamps, value_dict ) ):
-				event_matrix[s_sym].ix[ldt_timestamps[i]] = 1
-				discrete_events.append( DiscreteEvent( i, s_sym, f_symprice_today ) )
-
-	sorted_discrete_events = sorted( discrete_events, key = lambda e: ldt_timestamps[ e.timestamp_index ] )
-
-	return event_matrix, sorted_discrete_events
-
-def original_qualifier( i, ldt_timestamps, value_dict ):
-	bollinger_values = value_dict[ "bollinger values" ]
-	spy_bollinger_values = value_dict[ "spy bollinger values" ]
-	if i > 1:
-		if bollinger_values[ldt_timestamps[ i - 1 ] ] >= -2.0 \
-			and bollinger_values[ldt_timestamps[ i ] ] < -2.0 \
-			and spy_bollinger_values[ldt_timestamps[ i ] ] >= 1.1:
-				return True
-	return False
-
-class BollingerLTThresholdQualifierBuilder:
-	def __init__( self, threshold ):
-		self.threshold = threshold
-	
-	def qualify( self, i, ldt_timestamps, value_dict ):
-		bollinger_values = value_dict[ "bollinger values" ]
-		return bollinger_values[ ldt_timestamps[ i ] ] <= self.threshold
-
-class ClosingPriceRatioLTThresholdQualifierBuilder:
-	def __init__( self, threshold ):
-		self.threshold = threshold
-
-	def qualify( self, i, ldt_timestamps, value_dict ):
-		closing_prices = value_dict[ "close prices" ]
-		if i < 1:
-			return false
-		return ( closing_prices[ ldt_timestamps[ i ] ] / closing_prices[ ldt_timestamps[ i - 1 ] ] ) < self.threshold
 
 def convert_events_to_orders_by_share_amount( events, ldt_timestamps, trading_days_to_sell_delta = 5, shares_to_transact = 100 ):
 	orders = []
@@ -154,14 +73,14 @@ if __name__ == '__main__':
 		d_data[s_key] = d_data[s_key].fillna(method='bfill')
 		d_data[s_key] = d_data[s_key].fillna(1.0)
 
-	# q = ClosingPriceRatioLTThresholdQualifierBuilder( 0.9 ).qualify
-	# q = ClosingPriceRatioLTThresholdQualifierBuilder( 0.98 ).qualify
-	# q = ClosingPriceRatioLTThresholdQualifierBuilder( 0.95 ).qualify
-	q = ClosingPriceRatioLTThresholdQualifierBuilder( 0.94 ).qualify
-	# q = BollingerLTThresholdQualifierBuilder( -3.0 ).qualify
-	# q = original_qualifier
+	# q = eventfinder.ClosingPriceRatioLTThresholdQualifierBuilder( 0.9 ).qualify
+	# q = eventfinder.ClosingPriceRatioLTThresholdQualifierBuilder( 0.98 ).qualify
+	# q = eventfinder.ClosingPriceRatioLTThresholdQualifierBuilder( 0.95 ).qualify
+	q = eventfinder.ClosingPriceRatioLTThresholdQualifierBuilder( 0.94 ).qualify
+	# q = eventfinder.BollingerLTThresholdQualifierBuilder( -3.0 ).qualify
+	# q = eventfinder.original_qualifier
 
-	event_matrix, discrete_events = find_events( ls_symbols, d_data, ldt_timestamps, q )
+	event_matrix, discrete_events = eventfinder.find_events( ls_symbols, d_data, ldt_timestamps, q )
 
 	for d in discrete_events:
 		print d.to_string( ldt_timestamps )
